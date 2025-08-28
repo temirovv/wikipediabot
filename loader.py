14a8e0ee9265e32457249b7850e681166a423159
temirovv <hammasi yaxshi bo'lmaydi ishonurila battar boladi>
2025-08-28 14:19:28 +0500

    token removed

--- loader.py
+++ loader.py
@@ -3,6 +3,6 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 
-TOKEN = "7596603800:AAGO6rgvQ66_ANu6R7iP9HI2WR1YeKOxQaw"
+TOKEN = ""
 dp = Dispatcher()
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
